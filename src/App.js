786076dc2d087d3d8d80bfd6f27fc9d47f13ddf0
1amanton <iamantongit@gmail.com>
2022-08-26 15:20:03 +0300

    CRA

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Msg } from './components/Msg';
+
+
+const App = () => {
+  return (
+      <div className="App">
+        <Msg/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Msg.jsx
@@ -0,0 +1,7 @@
+export const Msg = () => {
+    return (
+        <div>
+            message
+        </div>
+    )
+}
